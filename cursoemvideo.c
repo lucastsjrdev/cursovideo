cc2c2f31fadd10f34a62d611962e2931fa652eb0
lucastsjrdev <lucastsjr96@gmail.com>
2026-03-19 13:50:35 +0000

    declarando variaveis

--- cursoemvideo.c
+++ cursoemvideo.c
@@ -1,13 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> //essa linha ''puxa'' ou ''chama'' a biblioteca. existem varias
 
+// curso intelectual tec
 int main (){
+int num = 10; //atribuindo valor
+float n2 = 6.79; //variavel com decimais
+char letra = 'a'; //variavel com caracter
 
 printf("Hello, World!\n");
+printf("Exibindo o numero inteiro %d.", num);
+printf("Exibindo o numero com decemais %.2f.", n2);
+printf("Exibindo a letra ou o caracter %s", letra);
 
 return 0;
 
 
-
-
-
 }
+//apenas comentários
+//usaria <stdlib.h> e a função system("pause") - gera a mensagem de pressionar tecla para continuar
+//se eu estivesse usando alguma ide mais antiga como o devc++ por exemplo
+//variaveis nao podem ter ''ç'', acentos gráficos ou espaços
+
+
+
+
+//
