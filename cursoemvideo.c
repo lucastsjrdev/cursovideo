8d23caf9ed707b0990debd076e2395b7a0d6ab1e
lucastsjrdev <lucastsjr96@gmail.com>
2026-03-19 14:03:43 +0000

    mexi

--- cursoemvideo.c
+++ cursoemvideo.c
@@ -3,14 +3,19 @@
 // curso intelectual tec
 int main (){
 int num = 10; //atribuindo valor
-float n2 = 6.79; //variavel com decimais
-char letra = 'a'; //variavel com caracter
+float n2 = 6.79; //variavel com decimais. mais precisa que int e menos que double
+char letra = 'a'; //variavel com caracter - usar %c para marcar
+char frase[10] = "Bom dia!"; //entre colchetes ou chaves n sei o limite de caracteres
+double n3 = 1.23456; //permite maior precisão armazenando mais casas do que float
 
 printf("Hello, World!\n");
-printf("Exibindo o numero inteiro %d.", num);
-printf("Exibindo o numero com decemais %.2f.", n2);
-printf("Exibindo a letra ou o caracter %s", letra);
-
+printf("%s \n", frase);
+printf("Exibindo o numero inteiro %d.\n", num);
+printf("Exibindo o numero com decemais %.2f.\n", n2);
+printf("Exibindo a letra ou o caracter %c.\n", letra);
+printf("Exibindo o numero do tipo double: %f.", n3);
+printf("Valores: %d, %.2f, %c, %s, %f", num, n2, letra, frase, n3);
+    
 return 0;
 
 
