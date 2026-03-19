c9336312d77ca649efd93f4fb2b675ee3954ba62
lucastsjrdev <lucastsjr96@gmail.com>
2026-03-19 17:09:37 +0000

    indo para if else

--- cursoemvideo.c
+++ cursoemvideo.c
@@ -6,16 +6,57 @@ int num = 10; //atribuindo valor
 float n2 = 6.79; //variavel com decimais. mais precisa que int e menos que double
 char letra = 'a'; //variavel com caracter - usar %c para marcar
 char frase[10] = "Bom dia!"; //entre colchetes ou chaves n sei o limite de caracteres
-double n3 = 1.23456; //permite maior precisão armazenando mais casas do que float
+double n3 = 1.234567; //permite maior precisão armazenando mais casas do que float
+int valor1, valor2, soma, sub, mult, numero, resto;
+float divi;
 
+/*
+//usando a função print para mostrar valores com variaveis distintas;
 printf("Hello, World!\n");
-printf("%s \n", frase);
+printf("%s. \n", frase);
 printf("Exibindo o numero inteiro %d.\n", num);
 printf("Exibindo o numero com decemais %.2f.\n", n2);
 printf("Exibindo a letra ou o caracter %c.\n", letra);
-printf("Exibindo o numero do tipo double: %f.", n3);
-printf("Valores: %d, %.2f, %c, %s, %f", num, n2, letra, frase, n3);
-    
+printf("Exibindo o numero do tipo double: %f.\n", n3);
+printf("Valores: %d, %.2f, %c, %s, %f. \n", num, n2, letra, frase, n3);
+
+printf("Digite um numero inteiro: \n", valor1);
+scanf("%d", &valor1);
+
+printf("Digite um segundo numero: \n", valor2);
+scanf("%d", &valor2);
+
+//declarando as funcoes aritmeticas
+soma = valor1 + valor2;
+sub = valor1 - valor2;
+mult = valor1 * valor2;
+divi = (float) valor1 / valor2;
+
+printf("O valor da soma de %d e %d é igual a: %d.\n", valor1, valor2, soma); //soma
+printf("O valor da subtracao de %d e %d é igual a: %d.\n", valor1, valor2, sub);
+printf("O valor da multiplicacao de %d e %d é igual a: %d.\n", valor1, valor2, mult);
+printf("O valor da divisao de %d e %d é igual a: %.2f.\n", valor1, valor2, divi);
+*/
+
+//testando sobras e fazendo um sistem que mostra se o numero é par ou impar
+
+printf("Digite um numero inteiro: \n", numero);
+scanf("%d", &numero);
+
+resto = numero % 2;
+
+printf("Resto da divisão: %d. \n", resto);
+//if - else//
+
+if (resto == 0){
+    printf("O numero é par!\n");
+} else {
+    printf("O numero é impar!\n");
+}
+
+
+
+
 return 0;
 
 
